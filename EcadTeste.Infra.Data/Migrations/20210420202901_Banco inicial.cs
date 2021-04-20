1b6c3d8f83ce67d014c92239332e618eebd701fb
Flavio Rianelli Lopes <flavio.rianelli@sidetech.com.br>
2021-04-20 17:46:43 -0300

    Inclusão do migrations

---
+++ EcadTeste.Infra.Data/Migrations/20210420202901_Banco inicial.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcadTeste.Infra.Data.Migrations
+{
+    public partial class Bancoinicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catagoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genero",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genero", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Autor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Codigo = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    IdCategoria = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Autor_Categoria",
+                        column: x => x.IdCategoria,
+                        principalTable: "Categoria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Musica",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Codigo = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    IdGenero = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musica", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Musica_Genero",
+                        column: x => x.IdGenero,
+                        principalTable: "Genero",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AutorMusica",
+                columns: table => new
+                {
+                    IdAutor = table.Column<Guid>(nullable: false),
+                    IdMusica = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutorMusica", x => new { x.IdAutor, x.IdMusica });
+                    table.ForeignKey(
+                        name: "FK_AutorMusica_Autor",
+                        column: x => x.IdAutor,
+                        principalTable: "Autor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AutorMusica_Musica",
+                        column: x => x.IdMusica,
+                        principalTable: "Musica",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Autor_IdCategoria",
+                table: "Autor",
+                column: "IdCategoria");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AutorMusica_IdMusica",
+                table: "AutorMusica",
+                column: "IdMusica");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Musica_IdGenero",
+                table: "Musica",
+                column: "IdGenero");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AutorMusica");
+
+            migrationBuilder.DropTable(
+                name: "Autor");
+
+            migrationBuilder.DropTable(
+                name: "Musica");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+
+            migrationBuilder.DropTable(
+                name: "Genero");
+        }
+    }
+}
